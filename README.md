6783a190c672be0c4fc090245f4d35509d97f8e5
Mike Kushnerik <mikekushnerik0@gmail.com>
2022-07-31 14:28:28 -0500

    Add hardware design files

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # pico-scd41-co2-meter
 Raspberry Pi Pico and Sensirion SCD41 CO2 Meter
 
+![Assembled Unit](https://github.com/miekush/pico-scd41-co2-meter/blob/main/pico_co2_monitor.JPG)
+
+# PCB
 ![Board Image](https://github.com/miekush/pico-scd41-co2-meter/blob/main/board_front.png)
--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-//7 segment wiring (adjust as needed):
+/* //7 segment wiring (adjust as needed):
 #define a_pin       6
 #define b_pin       7
 #define c_pin       8
@@ -27,7 +27,25 @@
 #define digit_1     13
 #define digit_2     14
 #define digit_3     15
-#define digit_4     16
+#define digit_4     16 */
+
+//7 segment wiring (adjust as needed):
+#define a_pin       14
+#define b_pin       7
+#define c_pin       10
+#define d_pin       13
+#define e_pin       15
+#define f_pin       12
+#define g_pin       8
+#define digit_1     16
+#define digit_2     11
+#define digit_3     9
+#define digit_4     6
+
+//rgb led pins:
+#define led_r       2
+#define led_g       1
+#define led_b       0
 
 //create mask for segment pins
 const uint32_t segment_mask =   (1 << a_pin) |
@@ -43,6 +61,10 @@ const uint32_t digit_mask =     (1 << digit_1) |
                                 (1 << digit_2) |
                                 (1 << digit_3) |
                                 (1 << digit_4);
+
+const uint32_t rgb_mask =       (1 << led_r) |
+                                (1 << led_g) |
+                                (1 << led_b);
 
 //LUT for segment pins
 //i.e. segment_pins[5] will display the value 5 on the display digit
@@ -88,12 +110,16 @@ bool timerFlag = false;
 //function prototypes
 void init_display(void);
 void update_display(uint16_t value);
+void init_rgb(void);
+void update_rgb(uint16_t value);
+
 bool timer_cb(struct repeating_timer *t);
 
 int main() 
 {
     stdio_init_all();
-    init_display();    
+    init_display();
+    init_rgb();    
 
     sensirion_i2c_hal_init();
 
@@ -110,6 +136,7 @@ int main()
     while(1)
     {
         update_display(co2);
+        update_rgb(co2);
 
         if(timerFlag)
         {
@@ -130,6 +157,15 @@ void init_display(void)
     gpio_init_mask(segment_mask | digit_mask);
     //set pins as output
     gpio_set_dir_out_masked(segment_mask | digit_mask);
+}
+
+//function to init the rgb led
+void init_rgb(void)
+{
+    //init the rgb output pins
+    gpio_init_mask(rgb_mask);
+    //set pins as output
+    gpio_set_dir_out_masked(rgb_mask);
 }
 
 //function to update the value of the 7 segment display
@@ -153,6 +189,32 @@ void update_display(uint16_t value)
         sleep_us(2000);
         i++;
     } while (value /= 10);
+}
+
+//function to update the air quality RGB led
+void update_rgb(uint16_t value)
+{
+    //good air quality
+    if(value < 800)
+    {
+        gpio_put(led_r, 0);
+        gpio_put(led_g, 1);
+        gpio_put(led_b, 0);
+    }
+    //average air quality
+    else if (value >= 800 && value < 1000)
+    {
+        gpio_put(led_r, 1);
+        gpio_put(led_g, 1);
+        gpio_put(led_b, 0);
+    }
+    //bad air quality
+    else
+    {
+        gpio_put(led_r, 1);
+        gpio_put(led_g, 0);
+        gpio_put(led_b, 0);        
+    }
 }
 
 //timer interrupt callback to poll scd41
